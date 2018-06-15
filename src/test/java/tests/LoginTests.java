93e8051a6f2c51e8b86f86539a972edfbd293a68
Ritesh <ryanisenia@hotmail.com>
2018-06-15 10:32:20 +0530

    initial

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTests {
+
+    @Test
+    public void logintest(){
+        System.out.println("testing");
+        Assert.assertTrue(true);
+
+}
+
+
+
+}
